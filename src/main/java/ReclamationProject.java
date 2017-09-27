d97a734ae76e59a4134ded56b0acf64b51b3ee86
David <davidbf2@illinois.edu>
2017-09-26 19:37:42 -0500

    Edited for readability

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -1,26 +1,42 @@
 /*
  * The following code needs a lot of TLC. So give it some!
- *
  * 1. Fix all checkstyle errors
  * 2. Determine what it does (it's going to be tough before you finish #1)
  * 3. Improve the name of the methods and variables
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
+/**
+ * finds longest substring between two strings.
+ */
+public class ReclamationProject {
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
-         */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+    /**
+     * Finds the longest common substring between a and b.
+     *
+     * @param a 1st input string
+     * @param b 2nd input string
+     * @return returns longest common substring
+     */
+
+    static String findSubstring(final String a, final String b) {
+        int aLen = a.length(), bLen = b.length();
+        String str1 = a, str2 = b;
+        if (aLen > bLen) {
+            str1 = b;
+            str2 = a;
+        }
+        String r = "";
+
+        for (int i = 0; i < aLen; i++) {
+            for (int j = bLen - i; j > 0; j--) {
+                for (int k = 0; k < bLen - j; k++) {
+                    if (str1.regionMatches(i, str2, k, j) && j > r.length()) {
+                        r = a.substring(i, i + j);
+                    }
+                }
+            }
+        }
+        return r;
+    }
 }
